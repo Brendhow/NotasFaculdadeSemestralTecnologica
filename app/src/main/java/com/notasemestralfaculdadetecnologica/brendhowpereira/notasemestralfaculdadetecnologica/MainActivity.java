bf07bfd98477e019964ee88b289c52eaa90afa90
brendhowpereira <brendhowpereira@hotmail.com>
2018-09-14 19:22:59 -0300

    Remove Icon Calculate

--- app/src/main/java/com/notasemestralfaculdadetecnologica/brendhowpereira/notasemestralfaculdadetecnologica/MainActivity.java
+++ app/src/main/java/com/notasemestralfaculdadetecnologica/brendhowpereira/notasemestralfaculdadetecnologica/MainActivity.java
@@ -1,13 +1,24 @@
 package com.notasemestralfaculdadetecnologica.brendhowpereira.notasemestralfaculdadetecnologica;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Vibrator vibracall;
+    String ata = getString(R.string.textViewResult);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vibracall = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void calcular(View view){
+        vibracall.vibrate(199);
     }
 }
